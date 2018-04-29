7ccab1bb2e89088f091b5e0b5a736832a506c213
hwiVeloper <zziller03@gmail.com>
2018-04-30 01:01:47 +0900

    인증관련 준비

---
+++ src/main/java/com/datemap/controller/SignController.java
@@ -0,0 +1,48 @@
+package com.datemap.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class SignController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		logger.info("===== GET login");
+		
+		return "login";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(HttpRequest req, Model model) {
+		logger.info("===== POST login");
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String join(Model model) {
+		logger.info("===== GET join");
+		
+		return "join";
+	}
+	
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	public String join(HttpRequest req, Model model) {
+		logger.info("===== POST join");
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(RedirectAttributes ra, Model model) {
+		return "home";
+	}
+}
---
+++ src/main/java/com/datemap/dto/MemberDTO.java
@@ -0,0 +1,52 @@
+package com.datemap.dto;
+
+public class MemberDTO {
+	private String id;
+	private String password;
+	private String name;
+	private String nickname;
+	private String birthday;
+	private Integer sex;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public Integer getSex() {
+		return sex;
+	}
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+	@Override
+	public String toString() {
+		return "MemberDTO [id=" + id + ", password=" + password + ", name=" + name + ", nickname=" + nickname
+				+ ", birthday=" + birthday + ", sex=" + sex + "]";
+	}
+}
