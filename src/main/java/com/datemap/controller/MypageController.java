8ddb857e642377d75e25477d2ee86baf3add0ec2
Jonghwi Lee <zziller03@gmail.com>
2018-06-10 18:28:06 +0900

    마이페이지 준비

---
+++ src/main/java/com/datemap/controller/MypageController.java
@@ -0,0 +1,10 @@
+package com.datemap.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MypageController {
+	private static final Logger logger = LoggerFactory.getLogger(MypageController.class);
+}
---
+++ src/main/java/com/datemap/vo/MyPageListVO.java
@@ -0,0 +1,61 @@
+package com.datemap.vo;
+
+public class MyPageListVO {
+	private String id;
+	private String name;
+	private String nickname;
+	private String title;
+	private String content;
+	private String postIdx;
+	private String mapId;
+	private String placeName;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPostIdx() {
+		return postIdx;
+	}
+	public void setPostIdx(String postIdx) {
+		this.postIdx = postIdx;
+	}
+	public String getMapId() {
+		return mapId;
+	}
+	public void setMapId(String mapId) {
+		this.mapId = mapId;
+	}
+	public String getPlaceName() {
+		return placeName;
+	}
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+}
